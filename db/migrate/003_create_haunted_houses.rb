8abc611e1e16e0315ed1f23597710d51f8b59012
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-05 21:39:23 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,10 +8,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      
+
+
       t.timestamps
     end
   end
